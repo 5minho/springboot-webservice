cc50b466914d1dc8fd4f45619dd88c7cdcc6ec5e
5minho <5minhominho@gmail.com>
2020-11-11 00:18:58 +0900

    어노테이션을 사용해서 중복코드 제거

--- src/main/java/me/minho/springboot/web/IndexController.java
+++ src/main/java/me/minho/springboot/web/IndexController.java
@@ -1,6 +1,7 @@
 package me.minho.springboot.web;
 
 import lombok.RequiredArgsConstructor;
+import me.minho.springboot.config.auth.LoginUser;
 import me.minho.springboot.config.auth.dto.SessionUser;
 import me.minho.springboot.service.PostsService;
 import me.minho.springboot.web.dto.PostsResponseDto;
@@ -9,19 +10,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpSession;
-
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
